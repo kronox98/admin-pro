c9d25de5e40ed641dc71ff61899fa0961ee873f8
Sergio Ayala <mg98sergio@gmail.com>
2020-10-28 22:30:45 -0500

    Fin seccion 8

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -16,27 +16,33 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: DashboardComponent
+        component: DashboardComponent,
+        data: { title : 'Dashboard'}
       },
       {
         path: 'progress',
-        component: ProgressComponent
+        component: ProgressComponent,
+        data: { title : 'Progress'}
       },
       {
         path: 'grafica1',
-        component: Grafica1Component
+        component: Grafica1Component,
+        data: { title : 'Grafica 1'}
       },
       {
         path: 'account',
-        component: AccountSettingsComponent
+        component: AccountSettingsComponent,
+        data: { title : 'Ajustes cuenta'}
       },
       {
         path: 'promesas',
-        component: PromesasComponent
+        component: PromesasComponent,
+        data: { title : 'Promesas'}
       },
       {
         path: 'rxjs',
-        component: RxjsComponent
+        component: RxjsComponent,
+        data: { title : 'Rxjs'}
       },
     ]
   },
--- src/app/share/breadcrumb/breadcrumb.component.ts
+++ src/app/share/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map } from "rxjs/operators";
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events
+    .pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map( (event: ActivationEnd) => event.snapshot.data)
+    )
+    .subscribe( data => {
+      console.log(data);
+      this.title = data.title;
+    })
   }
 
 }
